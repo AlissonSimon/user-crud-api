79318537c6af635cac5bffc3429442120bb6fd8e
Alisson Simon <alissonsimon21@gmail.com>
2026-03-09 17:19:25 -0300

    feat: creating ControllerExceptionHandler

---
+++ src/main/java/com/crud/demo/infra/ControllerExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.crud.demo.infra;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardError> messageNotReadableHandler(HttpServletRequest request) {
+        String error = "Body request error";
+        String message = "Verify JSON request again";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, message, request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFoundHandler(HttpServletRequest request) {
+        String error = "Resource not found error";
+        String message = "Verify if the resource exists";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, message, request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandardError> generalExceptionHandler(Exception e, HttpServletRequest request) {
+        String error = "Internal application error";
+        String message = e.getMessage();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, message, request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
